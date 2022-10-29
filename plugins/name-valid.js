fd9d4e6e9c4f6ce7a3614c568b0edadd61adbca8
Siddhesh Chavan <siddhesh.aprilinnovations@gmail.com>
2022-10-29 19:28:10 +0530

    fix:code-refactor

--- plugins/name-valid.js
+++ plugins/name-valid.js
@@ -1,22 +1,21 @@
 export default {
-    install: (app, options) => {
-      app.component("my-header", MyHeader);
-  
-      app.directive("font-size", (el, binding, vnode) => {
-        var size = 16;
-        switch (binding.arg) {
-          case "small":
-            size = 12;
-            break;
-          case "large":
-            size = 24;
-            break;
-          default:
-            size = 18;
-            break;
-        }
-        el.style.fontSize = size + "px";
-      });
-    },
-  };
-  
+  install: (app) => {
+    app.component("my-header", MyHeader);
+
+    app.directive("font-size", (el, binding) => {
+      var size = 16;
+      switch (binding.arg) {
+        case "small":
+          size = 12;
+          break;
+        case "large":
+          size = 24;
+          break;
+        default:
+          size = 18;
+          break;
+      }
+      el.style.fontSize = size + "px";
+    });
+  },
+};
--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -8,21 +8,27 @@ import en from "vee-validate/dist/locale/en.json";
 import * as rules from "vee-validate/dist/rules";
 import Vue from "vue";
 
-// Add a rule.
+/**
+ * Add rules to object
+ * @param rule the rule will be added to the object
+ */
 Object.keys(rules).forEach((rule) => {
   extend(rule, rules[rule]);
 });
 
 localize("en", en);
+/**
+ * Generic rule messages
+ */
 localize({
   en: {
-    messages: {
-      // generic rule messages...
-    },
+    messages: {},
   },
 });
 
-// Register it globally
+/**
+ * Register gloabally
+ */
 Vue.component("ValidationObserver", ValidationObserver);
 Vue.component("ValidationProvider", ValidationProvider);
 Vue.config.productionTip = false;
